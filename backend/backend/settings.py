61ba59ca1ce373053b076ffe53730408234730bf
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2025-01-13 00:39:29 +0800

    addressed deployed cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,7 +136,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://127.0.0.1:3000'  
+    'http://127.0.0.1:3000',
+    'https://pookie-rookies.web.app'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True  # Restrict this in production
