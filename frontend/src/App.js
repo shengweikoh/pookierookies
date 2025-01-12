521bef86a2a2779107386f317805eac150e3f9db
yuanqiyq <yuanqi.yong.2023@scis.smu.edu.sg>
2025-01-12 13:08:43 +0800

    edited sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/login/LoginPage'
 import Home from './pages/home/Home'
+import Account from './pages/account/Account.jsx'
+import Calendar from './pages/calendar/Calendar.jsx'
+import ManagePeople from './pages/manage_people/manage_people.jsx'
+import AssignTask from './pages/assign_task/Assign_Task.jsx'
+import ScheduleMeeting from './pages/scheduling/ScheduleMeeting.jsx'
+import GenerateSummary from './pages/summary/GenerateSummary.jsx'
+
 // import Landing from './pages/LandingPage';
 // import logo from './logo.svg';
 // import './App.css';
@@ -36,6 +43,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/tools/manage-people" element={<ManagePeople />} />
+          <Route path="/tools/assign-task" element={<AssignTask />} />
+          <Route path="/tools/schedule-meeting" element={<ScheduleMeeting />} />
+          <Route path="/tools/generate-summary" element={<GenerateSummary />} />
         </Routes>
       </div>
     </Router>
