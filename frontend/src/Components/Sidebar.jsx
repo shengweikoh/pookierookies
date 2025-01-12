b5db5f4939b17ff57689ebf5b13a40e60596990e
ryan neo jia hao <ryandacow@ryans-MacBook-Air.local>
2025-01-12 16:28:32 +0800

    Fix conflict 2

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 // import "../pages/home/Home.css";
 import "./Sidebar.css";
-import { Link } from "react-router-dom"
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="logo"><Link id="logo" to="/home">BookBuddy</Link></div>
+      <div className="logo">BookBuddy</div>
       <ul className="nav-list">
-        {/* <li className="nav-item section-title"></li> */}
-        <li className="nav-item section-title"><Link to="/account" className="links">Account</Link></li>
-        <li className="nav-item section-title"><Link to="/calendar" className="links">Calendar</Link></li>
-        <li className="nav-item section-title" id="tools">Tools</li>
-        <li className="nav-item2"><Link to="/tools/manage-people" className="links">Manage People</Link></li>
-        <li className="nav-item2"><Link to="/tools/assign-task" className="links">Assign Task</Link></li>
-        <li className="nav-item2"><Link to="/tools/schedule-meeting" className="links">Schedule Meeting</Link></li>
-        <li className="nav-item2"><Link to="/tools/generate-summary" className="links">Generate Summary</Link></li>
+        <li className="nav-item section-title">Home</li>
+        <li className="nav-item section-title">Account</li>
+        <li className="nav-item section-title">Calendar</li>
+        <li className="nav-item section-title">Tools</li>
+        <li className="nav-item2">Manage People</li>
+        <li className="nav-item2">Assign Task</li>
+        <li className="nav-item2">Schedule Meeting</li>
+        <li className="nav-item2">Generate Summary</li>
       </ul>
     </div>
   );
