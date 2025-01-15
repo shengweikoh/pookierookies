0af7cc6ebd0e90a552494572241ab41694506150
zuweii <zuwei.tan.2023@scis.smu.edu.sg>
2025-01-16 00:57:05 +0800

    debug 2

--- frontend/src/pages/assign_task/Assign_Task.jsx
+++ frontend/src/pages/assign_task/Assign_Task.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect } from "react";
+// import { useMemo } from "react";
 import axios from "axios";
 import Sidebar from "../../Components/Sidebar"; // Reusable Sidebar component
 import AssignNewTaskPopUp from "./AssignNewTaskPopUp";
