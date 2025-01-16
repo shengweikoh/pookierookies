20e19fe0785b5d903f5f44a88f4c071dc35c16eb
shengweikoh <shengweikoh.2023@scis.smu.edu.sg>
2025-01-16 23:24:14 +0800

    returned

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,8 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-#ev0!-o*$h=+pq%zvbolhh^8d$)a8)q)!iso$4s4sg)feg9y)u"
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_TRUSTED_ORIGINS = ['https://pookierookies-backend.duckdns.org', 'http://127.0.0.1:8000']
